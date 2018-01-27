630e0f5b9400d88df7654f06c3b95d745f60d883
James9446 <jameshreynolds@gmail.com>
2018-01-26 21:58:24 -0800

    Implemented app methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,7 @@ $(() => {
   app.init();
 });
 
+// var _ = underscore;
 var app = {};
 
 app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
@@ -11,24 +12,21 @@ app.init = function () {
 
   // var loadMessages = function loadMessages () {
 
-  //   // console.log('Loading messages');
-  //   // var $message = $('<p></p>');
+  var data = app.fetch('', () => {
+    var messages = data.responseJSON.results;
 
-  //   var messages = everyMessage.responseJSON.results;
-
-  //   for (let i = 0; i < messages.length; i++) {
-  //     $message.append(messages[i].text);
-  //     $('#every-message').append($message);
-  //     $message = $('<p></p>');
-  //   }
+    for (let i = 0; i < messages.length; i++) {
+      app.renderMessage(messages[i]);
+    }
+  });
   // };
 
-  var everyMessage = $.ajax({
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-    type: 'GET'
-  });
+  // var everyMessage = $.ajax({
+  //   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  //   type: 'GET',
+  //   success: () => { loadMessages(); }
+  // });
 
-  app.fetch();
 
 /*,
     success: loadMessages*/
@@ -52,11 +50,41 @@ app.send = function send (message) {
   });
 };
 
-app.fetch = function fetch (data) {
+app.fetch = function fetch (data, success = console.log) {
   data = data ? '/' + data : '';
   return $.ajax({
     type: 'GET',
+    data: 'order=-createdAt',
+    contentType: 'application/json',
     url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages' + data,
-    success: console.log // put a function here that returns what we ask for
+    success: success
   });
 };
+
+app.clearMessages = function clearMessages() {
+  $('#chats').children().remove();
+};
+
+app.renderMessage = function renderMessage(message) {
+  var $message = $('<div class="message"></div>');
+  var $username = $(`<h2 class="username" id="${_.escape(message.username)}"> ${_.escape(message.username)} </h2>`);
+  var $text = $(`<p class="text"> ${_.escape(message.text)} </p>`);
+
+  if (!message.roomname) {
+    message.roomname = '';
+  }
+  var $roomname = $(`<h3 class="roomname"> ${_.escape(message.roomname)} </h3>`);
+  $message.append($username, $text, $roomname);
+  $('#chats').append($message);
+};
+
+app.renderRoom = function renderRoom(roomname) {
+  var $room = $(`<div id=" ${roomname} "> ${roomname} </div>`);
+  $('#roomSelect').append($room);
+};
+
+app.handleUsernameClick = function handleUsernameClick () {
+
+};
+
+
