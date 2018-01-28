a6501d0fe5d7b35d50d5eb2f143f1973dce39200
James9446 <jameshreynolds@gmail.com>
2018-01-28 14:53:32 -0800

    Implemented interface

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,16 @@
 // YOUR CODE HERE:
 $(() => {
   app.init();
+  $('#chat-header').on('click', function (event) {
+    console.log('click event', event);
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data('whatever'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('.modal-title').text('New message to ' + recipient);
+    modal.find('.modal-body input').val(recipient);
+  });
 });
 
 // var _ = underscore;
@@ -109,5 +119,7 @@ app.handleSubmit = function handleSubmit() {
     app.send(message);
   });
 };
+
+
 
 
